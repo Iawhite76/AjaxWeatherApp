dfb573907bbde1a2634bce321d12bf50618a41bd
Ian White <ianwhite@utexas.edu>
2014-03-20 19:52:09 -0500

    log city temp and store in database SUCCESSFULL

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 WeatherApp::Application.routes.draw do
   resources :weather_cities
+  root "weather_cities#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
